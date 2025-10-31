1f492a9bfdd9d2840dd93234e380f7e5b7763d80
OlenaChkhan <olena1205chkhan@gmail.com>
2025-11-01 00:32:10 +0100

    add lesson03

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Lesson02 from "./lessons/Lesson02/Lesson02";
 // импортируем компонент Button для его дальнейшего использования
 // import Button from "./components/Button/Button";
+
+// Импорты лекций 
+import Lesson02 from "./lessons/Lesson02/Lesson02";
+import Lesson03 from "./lessons/Lesson03/Lesson03";
+
+
+// Импорты домашних работ
 
 function App() {
   return (
     <>
-     <Lesson02/>
+    {/* Лекция 2 */}
+      {/* <Lesson02/> */}
+      {/* Лекция 3 */}
+      <Lesson03 />
     </>
   );
 }
