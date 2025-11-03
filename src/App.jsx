24796587a5c08091393d8ef537cb88238a3d3417
OlenaChkhan <olena1205chkhan@gmail.com>
2025-11-03 22:48:26 +0100

    add Lesson4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 // импортируем компонент Button для его дальнейшего использования
 // import Button from "./components/Button/Button";
 
-// Импорты лекций 
+// Импорты лекций
 import Lesson02 from "./lessons/Lesson02/Lesson02";
 import Lesson03 from "./lessons/Lesson03/Lesson03";
-
+import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 // Импорты домашних работ
 
 function App() {
   return (
     <>
-    {/* Лекция 2 */}
+      {/* Лекция 2 */}
       {/* <Lesson02/> */}
+      
       {/* Лекция 3 */}
-      <Lesson03 />
+      {/* <Lesson03 /> */}
+
+      {/* Лекция 4 */}
+      <Lesson04 />
     </>
   );
 }
